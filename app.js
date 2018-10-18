dcb22d22667f6785929ebc7c684af4789302e427
Nicholas Tzavaras <nicholas_tzavaras@yahoo.com>
2018-10-18 17:59:58 -0600

    "Allowed control types"

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
 
